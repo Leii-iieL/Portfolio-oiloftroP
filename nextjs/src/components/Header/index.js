291d0ed9698ba009df74cc6f74df9a2c7c625636
Madison Robnett <silver.shine713@gmail.com>
2025-02-18 11:55:03 -0800

    fix errors

--- nextjs/src/components/Header/index.js
+++ nextjs/src/components/Header/index.js
@@ -1,15 +1,14 @@
 "use client"
 
-import react from "react";
 
 const Header = (props) => {
     console.log(props);
     const {title} = props;
-return(
-    <div>
-        <h1>{title}</h1>
-    </div>
-);
+    return(
+        <div>
+            <h1>{title}</h1>
+        </div>
+    );
 }
 
 export default Header;
