9041b7e7baa30b46e588f0c50fdfdc8947c2c3e3
Leii-iieL <silver.shine713@gmail.com>
2025-04-10 12:36:07 -0700

    idk what im doing

---
+++ nextjs/src/app/hamburger/hamburger.js
@@ -0,0 +1,33 @@
+const PROJECTS = [
+    {
+        id: "1bbd06f2-6441-40ce-a7cd-2838991b862c",
+        slug: "font-project",
+        title: "Font project",
+        description: "WOW, that resort sure is nice, too bad you're too broke to stay there.",
+        category: "print",
+        images: [
+            {
+                src: "",
+                title: "",
+                alt: "",
+            }
+        ],
+    },
+
+    {
+        id: "1bbd06f2-6441-40ce-a7cd-2838991b862d",
+        slug: "epic-boat-project",
+        title: "Epic boat project",
+        description: "Wow, wouldja lookat that lake, so very nice, i sure hope the titanic doesnt happen again..",
+        category: "web",
+        images: [
+            {
+                src: "",
+                title: "",
+                alt: "",
+            }
+        ],
+    }
+];
+
+export default PROJECTS;
